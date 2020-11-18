63bc62079e2ef731cde59821e69d4be3d284b299
Bruno Cesar Pasquini <39478173+brunocp76@users.noreply.github.com>
2020-11-18 12:22:31 -0300

    Começando o quarto exercício...

--- exercicios/04-boston-housing.R
+++ exercicios/04-boston-housing.R
@@ -2,7 +2,11 @@
 # Ajuste um MLP com quantas layers e unidades escondidas
 # você preferir.
 
+library(keras)
+
 base <- dataset_boston_housing()
+str(base)
+
 x <- base$train$x
 y <- base$train$y
 
