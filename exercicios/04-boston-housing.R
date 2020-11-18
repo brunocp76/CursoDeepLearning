ede7debbe2c7d1c72dc058ac2a84234485a714f7
Bruno Cesar Pasquini <39478173+brunocp76@users.noreply.github.com>
2020-11-18 17:01:59 -0300

    Tentando o exercício 4...

--- exercicios/04-boston-housing.R
+++ exercicios/04-boston-housing.R
@@ -10,14 +10,47 @@ str(base)
 x <- base$train$x
 y <- base$train$y
 
+str(x)
+str(y)
+
 # Model definition ---------------------------------------------
 
-input <- layer_input()
+input <- layer_input(shape = 13)
 
+output <- input %>% 
+   layer_dense(units = 13, activation = "relu") %>% 
+   layer_dense(units = 1)
+
+model <- keras_model(inputs = input, outputs = output)
+
+summary(model)
+
+model %>%
+   compile(
+      loss = loss_mean_squared_error,
+      optimizer = optimizer_sgd(lr = 0.01)
+   )
 
 # Model fitting ------------------------------------------------
 
+model %>%
+   fit(
+      x = x,
+      y = y,
+      batch_size = 64,
+      epochs = 8
+   )
 
+get_weights(model)
+predict(model, x)
 
+# Quick Graphical Check ---------------------------------------------------
 
+z <- dplyr::as_tibble(cbind(y, predict(model, x)))
 
+colnames(z) <- c("y", "y_hat")
+
+z %>%
+   ggplot2::ggplot(ggplot2::aes(x = y, y = y_hat)) +
+   ggplot2::geom_point(size = 0.9) +
+   ggplot2::ggtitle("Predicted x Realized")
