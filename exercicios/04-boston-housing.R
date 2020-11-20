23ee1fd6258c83348cde11a49376936a2255e328
Bruno Cesar Pasquini <39478173+brunocp76@users.noreply.github.com>
2020-11-20 01:17:32 -0300

    Depois da dica do Daniel que faltava normalizar os dados...

--- exercicios/04-boston-housing.R
+++ exercicios/04-boston-housing.R
@@ -10,8 +10,13 @@ str(base)
 x <- base$train$x
 y <- base$train$y
 
-str(x)
-str(y)
+
+# Dica do Milhão... Precisa escalar, principalmente os regressores...
+x2 <- scale(x)
+y2 <- scale(y)
+
+str(x2)
+str(y2)
 
 # Model definition ---------------------------------------------
 
@@ -19,8 +24,9 @@ input <- layer_input(shape = 13)
 
 output <- input %>% 
    layer_dense(units = 15, activation = "relu") %>% 
-   # layer_dense(units = 14, activation = "relu") %>% 
-   # layer_dense(units = 13, activation = "relu") %>% 
+   # layer_dense(units = 11, activation = "relu") %>%
+   layer_dense(units = 7, activation = "relu") %>%
+   # layer_dense(units = 3, activation = "relu") %>%
    layer_dense(units = 1)
 
 model <- keras_model(inputs = input, outputs = output)
@@ -30,17 +36,18 @@ summary(model)
 model %>%
    compile(
       loss = loss_mean_squared_error,
-      optimizer = optimizer_sgd(lr = 0.01)
+      optimizer = optimizer_sgd(lr = 0.0001)
    )
 
 # Model fitting ------------------------------------------------
 
 model %>%
    fit(
-      x = x,
-      y = y,
+      x = x2,
+      y = y2,
       batch_size = 2,
-      epochs = 20
+      epochs = 150,
+      validation_split = 0.25
    )
 
 get_weights(model)
@@ -48,11 +55,13 @@ predict(model, x)
 
 # Quick Graphical Check ---------------------------------------------------
 
-z <- dplyr::as_tibble(cbind(y, predict(model, x)))
+z <- dplyr::as_tibble(cbind(y2, predict(model, x2)))
 
 colnames(z) <- c("y", "y_hat")
 
 z %>%
-   ggplot2::ggplot(ggplot2::aes(x = y, y = y_hat)) +
+   ggplot2::ggplot(ggplot2::aes(x = y2, y = y_hat)) +
    ggplot2::geom_point(size = 0.9) +
-   ggplot2::ggtitle("Predicted x Realized")
+   ggplot2::ggtitle("Predicted x Realized") +
+   ggplot2::geom_abline(slope = 1, intercept = 0, col = "red")
+
